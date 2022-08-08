b25512c84d206adab0e2731dd0f26d6fb99c2446
masaki_yamanaka <ki.ymnka9@gmail.com>
2022-08-08 19:45:51 +0900

    Revert "edit config"
    This reverts commit 7f77f26c27275f56ceaee5be062c8536b09d748b.

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,1 +1,18 @@
-module.exports = require('.');
+module.exports = {
+  extends: [
+    "stylelint-config-recommended-scss",
+    "stylelint-config-recess-order",
+    "stylelint-rscss/config",
+    "stylelint-config-prettier",
+  ],
+  plugins: ["stylelint-scss"],
+  rules: {
+    "max-nesting-depth": [
+      2,
+      {
+        ignore: ["pseudo-classes"],
+        ignoreAtRules: ["include"],
+      },
+    ],
+  },
+};
